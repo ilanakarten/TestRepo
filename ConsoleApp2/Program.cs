7c1f77b4735a8b843b257cf318646b9b7550c6ed
Ilana Karten <ilana.karten@gmail.com>
2022-05-02 13:01:41 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+
+            var fileExtension = Path.GetExtension(dlg.FileName);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public ConcurrentDictionary<string, int> _tmp = new ConcurrentDictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _tmp["dd"] = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool s = false;
+            var tmp = string.IsNullOrEmpty(s.ToString());
+            //value = string.Join(";", col.Cast<string>().Where(s => !string.IsNullOrEmpty(s)).Distinct());
+
+        }
+    }
+}
+ 
